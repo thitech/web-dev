0d8afdf7caecbf4d9b60e738628e0922bdedf2da
krishnaraji-dev <krishnaraji.dev@gmail.com>
2025-06-15 22:47:55 +0530

    fixed asset prefix url

--- _next/static/chunks/pages/_app.js
+++ _next/static/chunks/pages/_app.js
@@ -1,5 +1,5 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/440e2f6697529b45.js",
-  "static/chunks/d395450ce4d519af.js",
-  "static/chunks/d4cdf2e515f4d569.js"
+  "static/chunks/ceb480be661c1fc5.js",
+  "static/chunks/2d89029a0870e65f.js"
 ])
--- _next/static/chunks/pages/_error.js
+++ _next/static/chunks/pages/_error.js
@@ -1,5 +1,5 @@
 __turbopack_load_page_chunks__("/_error", [
   "static/chunks/b52587224839defe.js",
-  "static/chunks/d395450ce4d519af.js",
-  "static/chunks/621688b349fbbf0e.js"
+  "static/chunks/ceb480be661c1fc5.js",
+  "static/chunks/8ed49f79b14edaff.js"
 ])
